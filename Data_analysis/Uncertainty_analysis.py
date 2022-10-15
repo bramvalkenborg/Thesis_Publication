49d54db2ae334796f0cfd07929f151b1e01ef651
bramvalkenborg <bramvalkenborg@gmail.com>
2022-10-15 14:52:56 +0200

    Long term and short term + output file

--- Data_analysis/Uncertainty_analysis.py
+++ Data_analysis/Uncertainty_analysis.py
@@ -15,11 +15,12 @@ from matplotlib.pyplot import cm
 # ----------------------------------------------------------------------------------------------------------------------
 # Main settings
 peat_min = 0.50
-area = 'Obi_River'
+area = 'NH'
 SIF_PAR = True
 Time_2021 = True
 SP_res = 0.2
 p = 0.05
+output_file = '/data/leuven/336/vsc33653/OUTPUT_pub/uncertainty_WTDopt.txt'
 # ----------------------------
 corr = '_c30_pt' + str(round(peat_min * 100)) + '_SPres' + str(round(SP_res*10)) + '_pValue' + str(int(round(p*100)))
 
@@ -133,21 +134,58 @@ for ilat in range(len(lat)):
 """
 
 # Bootstrap analysis
-WTDopt_mean = np.zeros((len(lat), len(lon)))
-WTD_CI_5 = np.zeros((len(lat), len(lon)))
-WTD_CI_95 = np.zeros((len(lat), len(lon)))
+WTDopt_s = np.zeros((len(lat), len(lon)))
+WTD_CI_5_s = np.zeros((len(lat), len(lon)))
+WTD_CI_95_s = np.zeros((len(lat), len(lon)))
 for ilat in range(len(lat)):
     print(str(ilat))
     for ilon in range(len(lon)):
         if not np.isnan(WTD).all() and not np.isnan(SIF_sAnom).all() and not np.isnan(WTD_sAnom).all():
-            WTDopt_mean_pix, WTD_CI_5_pix, WTD_CI_95_pix = Bootstrap_uncertainty(SIF_sAnom[ilat, ilon, :], WTD_sAnom[ilat, ilon, :], WTD[ilat, ilon, :], 10, p, time)
-            WTDopt_mean[ilat, ilon] = WTDopt_mean_pix
-            WTD_CI_5[ilat, ilon] = WTD_CI_5_pix
-            WTD_CI_95[ilat, ilon] = WTD_CI_95_pix
+            WTDopt_mean_pix_s, WTD_CI_5_pix_s, WTD_CI_95_pix_s = Bootstrap_uncertainty(SIF_sAnom[ilat, ilon, :], WTD_sAnom[ilat, ilon, :], WTD[ilat, ilon, :], 10, p, time)
+            WTDopt_s[ilat, ilon] = WTDopt_mean_pix_s
+            WTD_CI_5_s[ilat, ilon] = WTD_CI_5_pix_s
+            WTD_CI_95_s[ilat, ilon] = WTD_CI_95_pix_s
         else:
-            WTDopt_mean[ilat, ilon] = np.nan
-            WTD_CI_5[ilat, ilon] = np.nan
-            WTD_CI_95[ilat, ilon] = np.nan
+            WTDopt_s[ilat, ilon] = np.nan
+            WTD_CI_5_s[ilat, ilon] = np.nan
+            WTD_CI_95_s[ilat, ilon] = np.nan
 
-WTD_CI_5_mean = np.nanmean(WTD_CI_5)
-WTD_CI_95_mean = np.nanmean(WTD_CI_95)
+WTD_CI_5_mean_s = np.nanmean(WTD_CI_5_s)
+WTD_CI_95_mean_s = np.nanmean(WTD_CI_95_s)
+WTDopt_mean_s = np.nanmean(WTDopt_s)
+
+# Bootstrap analysis: Long term
+WTDopt_l = np.zeros((len(lat), len(lon)))
+WTD_CI_5_l = np.zeros((len(lat), len(lon)))
+WTD_CI_95_l = np.zeros((len(lat), len(lon)))
+for ilat in range(len(lat)):
+    print(str(ilat))
+    for ilon in range(len(lon)):
+        if not np.isnan(WTD).all() and not np.isnan(SIF_lAnom).all() and not np.isnan(WTD_lAnom).all():
+            WTDopt_mean_pix_l, WTD_CI_5_pix_l, WTD_CI_95_pix_l = Bootstrap_uncertainty(SIF_lAnom[ilat, ilon, :], WTD_lAnom[ilat, ilon, :], WTD[ilat, ilon, :], 10, p, time)
+            WTDopt_l[ilat, ilon] = WTDopt_mean_pix_l
+            WTD_CI_5_l[ilat, ilon] = WTD_CI_5_pix_l
+            WTD_CI_95_l[ilat, ilon] = WTD_CI_95_pix_l
+        else:
+            WTDopt_l[ilat, ilon] = np.nan
+            WTD_CI_5_l[ilat, ilon] = np.nan
+            WTD_CI_95_l[ilat, ilon] = np.nan
+
+WTD_CI_5_mean_l = np.nanmean(WTD_CI_5_l)
+WTD_CI_95_mean_l = np.nanmean(WTD_CI_95_l)
+WTDopt_mean_l = np.nanmean(WTDopt_l)
+
+with open(output_file, 'a') as f:
+    f.write('-------------------------------------------------------------\n')
+    f.write('Uncertainty WTDopt                  Date:'+str(datetime.datetime.today()))
+    f.write('-------------------------------------------------------------\n')
+    f.write('Short term analysis:\n')
+    f.write('Upper limit: '+str(WTD_CI_95_mean_s)+'\n')
+    f.write('WTDopt: ' + str(WTD_CI_95_mean_s) + '\n')
+    f.write('Lower limit: ' + str(WTD_CI_5_mean_s) + '\n\n')
+    f.write('Long term analysis:\n')
+    f.write('Upper limit: ' + str(WTD_CI_95_mean_l) + '\n')
+    f.write('WTDopt: ' + str(WTD_CI_95_mean_l) + '\n')
+    f.write('Lower limit: ' + str(WTD_CI_5_mean_l) + '\n')
+    f.write('-------------------------------------------------------------\n')
+    f.write('-------------------------------------------------------------\n\n')
