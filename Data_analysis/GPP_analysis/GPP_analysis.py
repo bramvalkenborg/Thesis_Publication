95fbb826c1a6497db354636fa262190d6402eebe
bramvalkenborg <bramvalkenborg@gmail.com>
2022-10-28 17:14:06 +0200

    update

--- Data_analysis/GPP_analysis/GPP_analysis.py
+++ Data_analysis/GPP_analysis/GPP_analysis.py
@@ -3,8 +3,8 @@ from validation_good_practice.ancillary.metrics import correct_n
 from patsy import dmatrices
 import statsmodels_adapted.api as sm
 
-write = True
-description = 'Resolution 8D'
+write = False
+description = 'Resolution 8D, GPP'
 input_file = 'CA_MER_GPP_analysis.csv'
 input_dir = '/data/leuven/336/vsc33653/Data/GPP_FluxTower/'
 output_file = '/data/leuven/336/vsc33653/OUTPUT_pub/GPP_output.txt'
@@ -36,15 +36,17 @@ GPPn_8D = GPP_8D/PAR_8D
 # Refill the timeseries to a daily resolution, this is needed for the anomaly calculations
 GPPn_1D = GPPn_8D.resample('1D').bfill()
 WTD_1D = WTD_8D.resample('1D').bfill()
+PAR_1D = PAR_8D.resample('1D').bfill()
 
 # Calculate the anomalies
 WTD_shortAnom = calc_anom(WTD_1D, longterm=False)
 GPPn_shortAnom = calc_anom(GPPn_1D, longterm=False)
+PAR_shortAnom = calc_anom(PAR_1D, longterm=False)
 
 # Change the resolution of the 1D anomalies to 8D anomalies
 WTD_shortAnom8d = WTD_shortAnom.resample('8D').first()
 GPPn_shortAnom8d = GPPn_shortAnom.resample('8D').first()
-
+PAR_shortAnom8d = PAR_shortAnom.resample('8D').first()
 
 # Model short term anomalies
 coef_s = np.zeros(2)
@@ -122,7 +124,9 @@ if write:
 
 GPPn_longAnom = calc_anom(GPPn_1D, longterm=True) - calc_anom(GPPn_1D, longterm=False)
 WTD_longAnom = calc_anom(WTD_1D, longterm=True) - calc_anom(WTD_1D, longterm=False)
+PAR_longAnom = calc_anom(PAR_1D, longterm=True) - calc_anom(PAR_1D, longterm=False)
 WTD_longAnom8d = WTD_longAnom.resample('8D').first()
+PAR_longAnom8d = PAR_longAnom.resample('8D').first()
 GPPn_longAnom8d = GPPn_longAnom.resample('8D').first()
 
 # Model long term anomalies
@@ -234,6 +238,8 @@ GPPn_clim_8D.plot(ax=ax3, color='grey', linestyle='--')
 WTD_8D.plot(ax=ax4, color='blue')
 WTD_seas_8D.plot(ax=ax4, color='grey')
 WTD_clim_8D.plot(ax=ax4, color='grey', linestyle='--')
+plt.hlines(WTD_opt_s, color='grey', linestyles=':', xmin=WTD_8D.index.min(), xmax=WTD_8D.index.max())
+plt.hlines(WTD_opt_l, color='slategray', linestyles='-.', xmin=WTD_8D.index.min(), xmax=WTD_8D.index.max())
 ax1.set_ylabel('GPP')
 ax2.set_ylabel('PAR')
 ax3.set_ylabel('GPP/PAR')
@@ -251,11 +257,63 @@ plt.xlabel('PAR')
 plt.ylabel('GPP')
 plt.colorbar()
 
+plt.figure()
+plt.scatter(PAR_shortAnom8d, GPPn_shortAnom8d)
+plt.xlabel('PAR_shortAnom')
+plt.ylabel('GPP_shortAnom')
+plt.xlim((-600, 600))
+plt.ylim((-4, 4))
+plt.grid()
+
+plt.figure()
+plt.scatter(PAR_longAnom8d, GPPn_longAnom8d)
+plt.xlabel('PAR_longAnom')
+plt.ylabel('GPP_longAnom')
+plt.grid()
+plt.xlim((-600, 600))
+plt.ylim((-4, 4))
+
+scenario_1_s = ((WTD_8D.dropna() > WTD_opt_s) & (WTD_shortAnom8d.dropna() > 0) & (GPPn_shortAnom8d.dropna() < 0))
+scenario_2_s = ((WTD_8D.dropna() > WTD_opt_s) & (WTD_shortAnom8d.dropna() < 0) & (GPPn_shortAnom8d.dropna() > 0))
+scenario_3_s = ((WTD_8D.dropna() < WTD_opt_s) & (WTD_shortAnom8d.dropna() > 0) & (GPPn_shortAnom8d.dropna() > 0))
+scenario_4_s = ((WTD_8D.dropna() < WTD_opt_s) & (WTD_shortAnom8d.dropna() < 0) & (GPPn_shortAnom8d.dropna() < 0))
+hypothesis_s = scenario_1_s | scenario_2_s | scenario_3_s | scenario_4_s
+count_s = hypothesis_s.value_counts()
+percentage_True_s = count_s[True] / (count_s[True] + count_s[False]) * 100
+
+scenario_1_l = ((WTD_8D.dropna() > WTD_opt_l) & (WTD_longAnom8d.dropna() > 0) & (GPPn_longAnom8d.dropna() < 0))
+scenario_2_l = ((WTD_8D.dropna() > WTD_opt_l) & (WTD_longAnom8d.dropna() < 0) & (GPPn_longAnom8d.dropna() > 0))
+scenario_3_l = ((WTD_8D.dropna() < WTD_opt_l) & (WTD_longAnom8d.dropna() > 0) & (GPPn_longAnom8d.dropna() > 0))
+scenario_4_l = ((WTD_8D.dropna() < WTD_opt_l) & (WTD_longAnom8d.dropna() < 0) & (GPPn_longAnom8d.dropna() < 0))
+hypothesis_l = scenario_1_l | scenario_2_l | scenario_3_l | scenario_4_l
+count_l = hypothesis_l.value_counts()
+percentage_True_l = count_l[True] / (count_l[True] + count_l[False]) * 100
+
+GPPn_shortAnom8d_True = GPPn_shortAnom8d.where(hypothesis_s==True, np.nan)
+GPPn_shortAnom8d_False = GPPn_shortAnom8d.where(hypothesis_s==False, np.nan)
+WTD_shortAnom8d_True = WTD_shortAnom8d.where(hypothesis_s==True, np.nan)
+WTD_shortAnom8d_False = WTD_shortAnom8d.where(hypothesis_s==False, np.nan)
+
+GPPn_longAnom8d_True = GPPn_longAnom8d.where(hypothesis_l==True, np.nan)
+GPPn_longAnom8d_False = GPPn_longAnom8d.where(hypothesis_l==False, np.nan)
+WTD_longAnom8d_True = WTD_longAnom8d.where(hypothesis_l==True, np.nan)
+WTD_longAnom8d_False = WTD_longAnom8d.where(hypothesis_l==False, np.nan)
+
 fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1, sharex=True)
-GPPn_shortAnom8d.plot.bar(ax=ax1, xticks=[], color='red')
-WTD_shortAnom8d.plot.bar(ax=ax2, xticks=[], color='blue')
-GPPn_longAnom8d.plot.bar(ax=ax3, xticks=[], color='red')
-WTD_longAnom8d.plot.bar(ax=ax4, xticks=[], color='blue')
+GPPn_shortAnom8d_True.plot.bar(ax=ax1, xticks=[], color='red')
+WTD_shortAnom8d_True.plot.bar(ax=ax2, xticks=[], color='blue')
+GPPn_shortAnom8d_False.plot.bar(ax=ax1, xticks=[], color='grey')
+WTD_shortAnom8d_False.plot.bar(ax=ax2, xticks=[], color='grey')
+
+GPPn_longAnom8d_True.plot.bar(ax=ax3, xticks=[], color='red')
+WTD_longAnom8d_True.plot.bar(ax=ax4, xticks=[], color='blue')
+GPPn_longAnom8d_False.plot.bar(ax=ax3, xticks=[], color='grey')
+WTD_longAnom8d_False.plot.bar(ax=ax4, xticks=[], color='grey')
+
+ax1.set_ylabel('GPPn_short')
+ax2.set_ylabel('WTD_short')
+ax3.set_ylabel('GPPn_long')
+ax4.set_ylabel('WTD_long')
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 norm_st = mpl.colors.TwoSlopeNorm(vmin=np.nanmin(WTD_8D), vcenter=WTD_opt_s,
